7d654260a79b52078140ed8664707d4657a6eb67
Denis Maslov <d.vi.maslov@gmail.com>
2016-04-06 13:08:00 +0300

    Завершена практика №8

---
+++ Labs/Lab8/Controllers/HomeController.cs
@@ -0,0 +1,80 @@
+﻿using Lab8.Models.MathSolvers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab8.Controllers
+{
+	public class HomeController : Controller
+	{
+		[HttpGet]
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult Index(string username = null)
+		{
+			if (username == "")
+			{
+				ViewBag.HelloText = "С анонимами не здороваюсь!";
+			}
+			else
+			{
+				ViewBag.HelloText = string.Format("Привет, {0}", username);
+			}
+			return View();
+		}
+
+		/// <summary>
+		/// Задание 1. Решает квадратное уравнение с заданными параметрами
+		/// </summary>
+		/// <param name="a">Коэфициент при x^2</param>
+		/// <param name="b">Коэфициент при x^1</param>
+		/// <param name="c">Коэфициент при x^0</param>
+		/// <returns>Массив корней уравнения</returns>
+		public string Solve(double a, double b, double c)
+		{
+			//http://localhost:49963/home/solve?a=1&b=0&c=1
+			string output = "";
+			ComplexNumber[] result = SquareSolver.Solve(a, b, c);
+
+			for (int i = 0; i < result.Length; i++)
+			{
+				output = string.Format("{2}x[{0}] = {1}<br />", i, result[i], output);
+			}
+
+			return output;
+		}
+
+		/// <summary>
+		/// Задание 2. Выводит на экран переданный через GET массив.
+		/// </summary>
+		/// <param name="arr">Массив для вывода.</param>
+		/// <returns>HTML-представление массива</returns>
+		public string PrintArray(string[] arr)
+		{
+			//http://localhost:49963/home/printarray?arr[0]=кот&arr[1]=пёс&arr[2]=скунс
+			string result = "<ol>";
+
+			if (arr != null)
+			{
+				foreach (string s in arr)
+				{
+					result = string.Concat(result, "<li>", s, "</li>");
+				}
+
+				result += "</ol>";
+			}
+			else
+			{
+				result = string.Format("Передайте в контроллер массив в формате: http://localhost:49963/home/printarray?arr[0]=кот&arr[1]=пёс&arr[2]=скунс");
+			}
+
+			return result;
+		}
+	}
+}
---
+++ Labs/Lab8/Models/MathSolvers/SquareSolver.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab8.Models.MathSolvers
+{
+	public static class SquareSolver
+	{
+		public static ComplexNumber[] Solve(double a, double b, double c)
+		{
+			double d;
+
+			d = b * b - 4 * a * c;
+			ComplexNumber[] x = new ComplexNumber[d == 0 ? 1 : 2];
+
+			if (d < 0)
+			{
+				x[0] = (new ComplexNumber(0, 1) * Math.Sqrt(-d) - b) / (2 * a);
+				x[1] = (new ComplexNumber(0, 1) * -Math.Sqrt(-d) - b) / (2 * a);
+			}
+			else if (d == 0)
+			{
+				x[0] = new ComplexNumber(-b / (2 * a), 0);
+			}
+			else
+			{
+				x[0] = new ComplexNumber((Math.Sqrt(d) - b) / (2 * a), 0);
+				x[1] = new ComplexNumber(-(Math.Sqrt(d) - b) / (2 * a), 0);
+			}
+			return x;
+		}
+	}
+}
