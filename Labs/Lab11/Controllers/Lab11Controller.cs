9374d23d0664a5888df9fe2c663d95343c3a6e7d
Denis Maslov <d.vi.maslov@gmail.com>
2016-04-18 14:47:15 +0300

    Начата практика №11

---
+++ Labs/Lab11/Controllers/Lab11Controller.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab11.Controllers
+{
+	public class Lab11Controller : Controller
+	{
+		public ActionResult Task1(int[] arr)
+		{
+			return View(arr);
+		}
+	}
+}
---
+++ Labs/Lab11/Helpers/ListHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab11.Helpers
+{
+	public static class ListHelper
+	{
+		public static MvcHtmlString CreateList(this HtmlHelper html, int[] items)
+		{
+			string result = "";
+			if (items != null)
+			{
+				TagBuilder ul = new TagBuilder("ul");
+				foreach (int i in items)
+				{
+					TagBuilder li = new TagBuilder("li");
+					li.SetInnerText(i.ToString());
+					ul.InnerHtml += li;
+				}
+
+				result = ul.ToString();
+			}
+			return new MvcHtmlString(result);
+		}
+	}
+}
