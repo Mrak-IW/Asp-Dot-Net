bb5e82e739bf7b16b01db765e744bc3898b0ce96
Mrak <mrak.iw@gmail.com>
2016-04-19 22:29:58 +0300

    Начата практика №12

---
+++ Labs/Lab12/Controllers/Lab12Controller.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab12.Controllers
+{
+	public class Lab12Controller : Controller
+	{
+		public ActionResult Task1()
+		{
+			return View();
+		}
+	}
+}
