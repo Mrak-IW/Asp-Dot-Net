8007d9325ffb5643abbc58db8f935267f061225a
Mrak <mrak.iw@gmail.com>
2016-04-24 23:12:01 +0300

    Закончена практика №12

--- Labs/Lab12/App_Start/RouteConfig.cs
+++ Labs/Lab12/App_Start/RouteConfig.cs
@@ -16,64 +16,81 @@ namespace Lab12
 			routes.IgnoreRoute("Lab12-Index/forbidden");
 
 			routes.MapRoute(
+				name: "Task14",
+				url: "NA/{controller}/{action}",
+				defaults: new { controller = "Lab12", action = "Index", routeName = "Маршрут Task1" },
+				namespaces: new[] { "Lab12.Areas.MyNewArea.Controllers" }
+			);
+
+			routes.MapRoute(
 				name: "Task10",
 				url: "{controller}/",
 				defaults: new { action = "SuperAction", routeName = "Маршрут Task10" },
-				constraints : new { controller = "^New.*" }
+				constraints: new { controller = "^New.*" },
+				namespaces: new[] { "Lab12.Controllers" }
 			);
 
 			routes.MapRoute(
 				name: "Task5",
 				url: "MySite/{controller}~{action}/",
-				defaults: new { routeName = "Маршрут Task5" }
+				defaults: new { routeName = "Маршрут Task5" },
+				namespaces: new[] { "Lab12.Controllers" }
 			);
 
 			routes.MapRoute(
 				name: "Task2",
 				url: "{controller}-{action}/{parameter}",
-				defaults: new { routeName = "Маршрут Task2" }
+				defaults: new { routeName = "Маршрут Task2" },
+				namespaces: new[] { "Lab12.Controllers" }
 			);
 
 			routes.MapRoute(
 				name: "Task8",
 				url: "{controller}/{action}/{param1}/{param2}",
-				defaults: new { routeName = "Маршрут Task8" }
+				defaults: new { routeName = "Маршрут Task8" },
+				namespaces: new[] { "Lab12.Controllers" }
 			);
 
 			routes.MapRoute(
 				name: "Task7",
 				url: "Task7",
-				defaults: new { controller = "Lab12", action = "Task7", routeName = "Маршрут Task7" }
+				defaults: new { controller = "Lab12", action = "Task7", routeName = "Маршрут Task7" },
+				namespaces: new[] { "Lab12.Controllers" }
 			);
 
 			routes.MapRoute(
 				name: "Task6",
 				url: "Main/Run",
-				defaults: new { controller = "Lab12", action = "Index", routeName = "Маршрут Task6" }
+				defaults: new { controller = "Lab12", action = "Index", routeName = "Маршрут Task6" },
+				namespaces: new[] { "Lab12.Controllers" }
 			);
 
 			routes.MapRoute(
 				name: "Task3",
 				url: "{controller}/",
-				defaults: new { action = "Task3", routeName = "Маршрут Task3" }
+				defaults: new { action = "Task3", routeName = "Маршрут Task3" },
+				namespaces: new[] { "Lab12.Controllers" }
 			);
 
 			routes.MapRoute(
 				name: "Task2Alt",
 				url: "{controller}/{action}/{parameter}",
-				defaults: new { routeName = "Маршрут Task2Alt" }
+				defaults: new { routeName = "Маршрут Task2Alt" },
+				namespaces: new[] { "Lab12.Controllers" }
 			);
 
 			routes.MapRoute(
 				name: "Task1",
 				url: "{controller}/{action}",
-				defaults: new { controller = "Lab12", action = "Index", routeName = "Маршрут Task1" }
+				defaults: new { controller = "Lab12", action = "Index", routeName = "Маршрут Task1" },
+				namespaces: new[] { "Lab12.Controllers" }
 			);
 
 			routes.MapRoute(
 				name: "Task9",
 				url: "{controller}/{action}/{*catchall}",
-				defaults: new { routeName = "Маршрут Task9" }
+				defaults: new { routeName = "Маршрут Task9" },
+				namespaces: new[] { "Lab12.Controllers" }
 			);
 		}
 	}
---
+++ Labs/Lab12/Areas/MyNewArea/Controllers/Lab12Controller.cs
@@ -0,0 +1,21 @@
+﻿using Lab12.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab12.Areas.MyNewArea.Controllers
+{
+    public class Lab12Controller : Controller
+    {
+        // GET: MyNewArea/Lab12
+        public ActionResult Index(string routeName, string parameter)
+        {
+			ViewInfo output = new ViewInfo();
+			output.routeName = routeName;
+			output.someText = "Контроллер из дополнительной области";
+			return View("/Views/Lab12/Index.cshtml", output);
+        }
+    }
+}
