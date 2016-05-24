f47de4f2685d41678b60ed4b66b75ea101e41aee
Denis Maslov <d.vi.maslov@gmail.com>
2016-05-24 11:40:17 +0300

    Завершена практика №16

---
+++ Labs/Lab16/Controllers/StaffController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Lab16.Models;
+
+namespace Lab16.Controllers
+{
+    public class StaffController : Controller
+    {
+        private CompanyContext db = new CompanyContext();
+
+        // GET: Staff
+        public ActionResult Index()
+        {
+            return View(db.Workers.ToList());
+        }
+
+        // GET: Staff/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Staff staff = db.Workers.Find(id);
+            if (staff == null)
+            {
+                return HttpNotFound();
+            }
+            return View(staff);
+        }
+
+        // GET: Staff/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Staff/Create
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Surname,Position")] Staff staff)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Workers.Add(staff);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(staff);
+        }
+
+        // GET: Staff/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Staff staff = db.Workers.Find(id);
+            if (staff == null)
+            {
+                return HttpNotFound();
+            }
+            return View(staff);
+        }
+
+        // POST: Staff/Edit/5
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Surname,Position")] Staff staff)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(staff).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(staff);
+        }
+
+        // GET: Staff/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Staff staff = db.Workers.Find(id);
+            if (staff == null)
+            {
+                return HttpNotFound();
+            }
+            return View(staff);
+        }
+
+        // POST: Staff/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Staff staff = db.Workers.Find(id);
+            db.Workers.Remove(staff);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
