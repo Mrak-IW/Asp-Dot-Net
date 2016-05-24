4f48196b80cf9fc4bada8e80042465971a80804d
Denis Maslov <d.vi.maslov@gmail.com>
2016-05-24 11:28:12 +0300

    Реализовано задание 16.1

--- Labs/Lab16/Controllers/HomeController.cs
+++ Labs/Lab16/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Lab16.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -15,14 +16,61 @@ namespace Lab16.Controllers
 			return View(db.Workers);
 		}
 
+		[HttpGet]
 		public ActionResult Add()
 		{
 			return View();
 		}
 
+		[HttpPost]
+		public ActionResult Add(Staff worker)
+		{
+			db.Workers.Add(worker);
+			db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+
+		[HttpGet]
+		public ActionResult Delete(int? id)
+		{
+			if (id == null)
+			{
+				return HttpNotFound();
+			}
+			Staff worker = db.Workers.Find(id);
+			if (worker == null)
+			{
+				return HttpNotFound();
+			}
+			return View(worker);
+		}
+
+		[HttpPost]
+		[ActionName("Delete")]
+		public ActionResult DeleteConfirmed(int id)
+		{
+			Staff worker = db.Workers.Find(id);
+			if (worker == null)
+			{
+				return HttpNotFound();
+			}
+			db.Workers.Remove(worker);
+			db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+
+		[HttpGet]
 		public ActionResult Edit(int id)
 		{
 			return View(db.Workers.Where(worker => worker.Id == id).FirstOrDefault());
+		}
+
+		[HttpPost]
+		public ActionResult Edit(Staff worker)
+		{
+			db.Entry(worker).State = EntityState.Modified;
+			db.SaveChanges();
+			return RedirectToAction("Index");
 		}
 	}
 }
